6ccccadc71573981325a0e2608f4df1eb21f84c3
shamik <39588365+Shamik-07@users.noreply.github.com>
2022-04-17 17:08:05 +0530

    Adding comments and changing some formatting.

--- decorators/debug_decorator.py
+++ decorators/debug_decorator.py
@@ -6,15 +6,18 @@ import functools
 def debug(func):
     """Print the function arguments and return value."""
     @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]                      
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  
-        signature = ", ".join(args_repr + kwargs_repr)           
+    def debug_wrapper(*args, **kwargs):
+        # repr to get a list of the function arguments
+        args_repr = [repr(a) for a in args]
+        # repr to get a list of keyword arguments and their
+        # corresponding values
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
         print(f"Calling {func.__name__}({signature})")
         value = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {value!r}")           
+        print(f"{func.__name__!r} returned {value!r}")
         return value
-    return wrapper_debug
+    return debug_wrapper
 
 
 math.factorial = debug(math.factorial)
