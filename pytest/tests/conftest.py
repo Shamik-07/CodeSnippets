975b10dbd89ec18b009c0997648dc5824c9250f4
shamik <39588365+Shamik-07@users.noreply.github.com>
2022-10-24 15:21:36 +0530

    Made changes to the conftest.py to change the return statements to yield and included tear down after it.

--- pytest/tests/conftest.py
+++ pytest/tests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def input_value():
    input = 39
-   return input
+   yield input
+   print("executing after yield statement as a tear down")
+   del input
 
 @pytest.fixture(scope="session")
 def generated_yyyy_mm_dd():
     yyyy, mm, dd = map(int, "1992/09/08".split("/"))
-    return yyyy, mm, dd
+    yield yyyy, mm, dd
+    print("executing after yield statement as a tear down")
+    del yyyy, mm, dd
