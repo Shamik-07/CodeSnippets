c278585709723aaef0da904ddfc3ef1c22d54576
shamik <39588365+Shamik-07@users.noreply.github.com>
2022-10-24 16:18:03 +0530

    Edited the conftex.py.

--- pytest/tests/conftest.py
+++ pytest/tests/conftest.py
@@ -4,12 +4,12 @@ import pytest
 def input_value():
    input = 39
    yield input
-   print("executing after yield statement as a tear down")
+   print("***executing after yield statement as a tear down***")
    del input
 
 @pytest.fixture(scope="session")
 def generated_yyyy_mm_dd():
     yyyy, mm, dd = map(int, "1992/09/08".split("/"))
     yield yyyy, mm, dd
-    print("executing after yield statement as a tear down")
+    print("***executing after yield statement as a tear down***")
     del yyyy, mm, dd
