b92f1cb7785a06571ad0d4fabe6fa8c099fdfa72
shamik <39588365+Shamik-07@users.noreply.github.com>
2025-01-14 16:37:22 +0530

    changing commented code in rust practice.

--- rust_practice/hello.rs
+++ rust_practice/hello.rs
@@ -1,3 +1,9 @@
+/*
+to run this file
+1. rustc hello.rs; this will generate a `hello` compiled file in the cd
+2. execute the file with `./hello`
+*/
 fn main(){
     println!("hello world");
+    println!("rust is awesome...");
 }
