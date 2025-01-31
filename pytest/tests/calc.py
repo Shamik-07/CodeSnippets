bd071f4f878db59be21ee83fbf82cde703fa3ac0
Shamik <39588365+Shamik-07@users.noreply.github.com>
2025-01-31 12:38:35 +0530

    Update calc.py
    Updating the pytest main file to include a new test to read data

--- pytest/tests/calc.py
+++ pytest/tests/calc.py
@@ -1,3 +1,7 @@
+import pytest
+import pickle
+from argparse import Namespace
+
 def calc(a, b, op):
     if op == "+":
         return a + b
@@ -8,3 +12,10 @@ def calc(a, b, op):
     elif op == "/":
         return a / b
     raise ValueError("Invalid operator")
+
+def read_data(args: Namespace) -> None:
+	with open(file=args.some_random_filepath, mode="rb") as f:
+        obj = pickle.load(f)
+	with open(file=args.another_random_filepath, mode="rb") as f:
+        obj_1 = pickle.load(f)
+	return
